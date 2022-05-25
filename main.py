425d051d6ff855bde7e8cfc3cbcc10a5ce43c3e6
Nishtha Agarwal <only4nishtha@gmail.com>
2022-05-25 23:23:34 +0530

    WebCam working

---
+++ main.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path = 'ImagesBasic'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cls in myList:
+    curImg = cv2.imread(f'{path}/{cls}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cls)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    facesCurrFrame = face_recognition.face_locations(imgS)
+    encodesCurrFrame = face_recognition.face_encodings(imgS,facesCurrFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurrFrame, facesCurrFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDist = face_recognition.face_distance(encodeListKnown,encodeFace)
+        print(faceDist)
+        matchIndex = np.argmin(faceDist)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
+
+
